14524fdb3e6ef2d81a3e807448cfabc126a89f1f
kpothuri <kpothuri@gmail.com>
2022-04-03 17:57:26 -0500

    Used Learn test script

--- deploy/Website.Tests.ps1
+++ deploy/Website.Tests.ps1
@@ -1,21 +1,23 @@
-param (
+param(
     [Parameter(Mandatory)]
     [ValidateNotNullOrEmpty()]
     [string] $HostName
 )
 
 Describe 'Toy Website' {
+
     It 'Serves pages over HTTPS' {
-        $request = [System.Net.WebRequest]::Create("https://$HostName")
+        $request = [System.Net.WebRequest]::Create("https://$HostName/")
         $request.AllowAutoRedirect = $false
-        $request.GetResponse().StatusCode | 
+        $request.GetResponse().StatusCode |
         Should -Be 200 -Because "the website requires HTTPS"
     }
 
     It 'Does not serves pages over HTTP' {
-        $request = [System.Net.WebRequest]::Create("http://$HostName")
+        $request = [System.Net.WebRequest]::Create("http://$HostName/")
         $request.AllowAutoRedirect = $false
-        $request.GetResponse().StatusCode |
+        $request.GetResponse().StatusCode | 
         Should -BeGreaterOrEqual 300 -Because "HTTP is not secure"
     }
+
 }
